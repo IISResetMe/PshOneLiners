7d6a69283ab20ef185968e53be8a9337e4473e06
IISResetMe <IISResetMe@users.noreply.github.com>
2014-12-06 15:47:46 +0100

    Update PoormansSingleton.ps1

--- PoormansSingleton.ps1
+++ PoormansSingleton.ps1
@@ -1,1 +1,1 @@
-(&{if($c-is[System.Net.WebClient]){$c}else{($c=New-Object System.Net.WebClient)}}).DownloadString("http://en.wikipedia.org/wiki/Lazy_initialization")
+(&{if($c-is[System.Net.WebClient]){$c}else{($Global:c=New-Object System.Net.WebClient)}}).DownloadString("http://en.wikipedia.org/wiki/Lazy_initialization")
